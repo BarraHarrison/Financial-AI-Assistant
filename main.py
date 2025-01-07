1e6ec5b94e0bda551bec09208b808dd89282271b
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-08 07:07:32 +0900

    Libraries to install in the requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # Financial AI Assistant in Python
+# Libraries to install:
+# pip install neuralintents
+# pip install matplotlib
+# pip install mplfinance
+# pip install pandas
+# pip install pandas-datareader

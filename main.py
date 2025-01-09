5cb30a341695791edebfc734ff7e9b51ba18a234
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-10 08:54:49 +0900

    add_portfolio and remove_portfolio functions created

--- main.py
+++ main.py
@@ -9,10 +9,34 @@ import pickle
 import sys
 import datetime as dt 
 
-portfolio = {'AAPL': 20,
-             'TSLA': 5,
-             'GS': 10
-             }
 
 with open('portfolio.pkl', 'wb') as f:
-    pickle.dump(portfolio, f)
+    portfolio = pickle.load(f)
+
+def save_portfolio():
+    with open('portfolio.pkl', 'wb') as f:
+        pickle.dump(portfolio, f)
+
+def add_portfolio():
+    ticker = input("Which stock do you want to add: ")
+    amount = input("How many shares do you want to add: ")
+
+    if ticker in portfolio.keys():
+        portfolio[ticker] += amount
+    else:
+        portfolio[ticker] = amount
+
+    save_portfolio()
+
+def remove_portfolio():
+    ticker = input("Which stock do you want to sell: ")
+    amount = input("How many shares do you want to sell: ")
+
+    if ticker in portfolio.keys():
+        if amount <= portfolio[ticker]:
+            portfolio[ticker] -= amount
+            save_portfolio()
+        else:
+            print("You don't have enough shares!")
+    else:
+        print(f"You don't own any shares of {ticker}")
